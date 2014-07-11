4bef57c6e4fc584bfd515afbc3ccb4c9eade2bc6
fgassert <cowbox314@gmail.com>
2014-07-11 00:56:37 -0400

    setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
-from distutils.core import setup
+from distutils.core import setup 
 from Cython.Build import cythonize
 import numpy
 
 setup(
     name = 'watershed',
     version = '0.1',
-    packages = find_packages(),
+    packages = ['watershed']
     ext_modules = cythonize('_watershed.pyx'),
     include_dirs = [numpy.get_include()],
     license = "MIT"
